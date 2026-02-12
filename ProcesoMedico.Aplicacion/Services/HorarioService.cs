bbf55064d7a654171c9a9cc0043934cc7b3ab82f
Steven Omar Cardenas Ortiz <steven.cardenas@viaxperta.com>
2026-02-12 03:56:34 -0500

    se agrega horario

---
+++ ProcesoMedico.Aplicacion/Services/HorarioService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProcesoMedico.Aplicacion.Interfaces;
+using ProcesoMedico.Dominio.Entities;
+using ProcesoMedico.Infraestructura.Interfaces;
+
+namespace ProcesoMedico.Aplicacion.Services
+{
+    public class HorarioService : GenericService<Horario>, IHorarioService
+    {
+        private readonly IGenericRepository<Horario> _repo;
+
+        public HorarioService(IGenericRepository<Horario> repo) : base(repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<int> InsertHorarioAsync(Horario Horario)
+        {
+            var spParams = new
+            {
+               Horario.HoraInicio,
+               Horario.HoraFin,
+               Horario.UsuarioCreacion
+            };
+
+            return await _repo.InsertAsync(Horario, spParams);
+        }
+
+        public async Task<int> UpdateHorarioAsync(Horario Horario)
+        {
+            var spParams = new
+            {
+                Horario.HoraInicio,
+                Horario.HoraFin,
+                Horario.UsuarioModificacion
+            };
+
+            return await _repo.UpdateAsync(Horario, spParams);
+        }
+    }
+}
